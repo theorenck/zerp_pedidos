6634a4770f112d04435200d955baa04142e0724e
LucioMarcel <luciomarcel@gmail.com>
2014-11-06 17:16:15 -0200

    mend

--- app/models/pedido.rb
+++ app/models/pedido.rb
@@ -1,5 +1,10 @@
 class Pedido < ActiveRecord::Base
-	has_one :cliente
-	has_one :vendedor
-	has_many :itens_pedido
+  has_one :cliente
+  has_one :vendedor
+  has_many :itens_pedido
+
+  enum situacao: { aberto: '1', finalizado: '2', cancelado: '3' }
+
 end
+
+
